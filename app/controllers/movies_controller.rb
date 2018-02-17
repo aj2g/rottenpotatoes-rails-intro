2c5e361c23f144733b377b03b35ef6bf17b33b85
aj2g <>
2018-02-17 02:03:07 -0600

    trying again after rake

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-      @movies = Movie.all
+      @hilite = session[:sort_by]
+      sort_by = session[:sort_by]
+      @movies = Movie.order(sort_by)
   end
 
   def new
